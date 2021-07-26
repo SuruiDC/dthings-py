2c5a5411ecd7a6b300322a5b109900981ac34be1
SuruiDC <73009341+SuruiDC@users.noreply.github.com>
2021-07-26 14:20:09 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name="dthings-py",
+	version="1.0.0",
+	description="Get information users and bots from discordthings.com",
+	author="Surui#5566",
+	packages=["functions"]
+	)
